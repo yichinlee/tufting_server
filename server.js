4837a99683d68e37f1f480748e6e06dd7be586b9
Yi-Chin Lee <ruby023123@gmail.com>
2022-09-23 17:06:51 -0400

    first

---
+++ server.js
@@ -0,0 +1,34 @@
+const WebSocket = require("ws");
+
+const PORT = 5050;
+
+const wsServer = new WebSocket.Server({
+  port: PORT,
+});
+
+
+
+wsServer.on("connection", function (socket) {
+  // Some feedback on the console
+  console.log("A client just connected");
+
+  // Attach some behavior to the incoming socket
+  socket.send("server says hi");
+  socket.on("message", function (msg) {
+    console.log("Received message from client: " + msg);
+    //socket.send(msg);//echo
+    var count = 0;
+
+    // Broadcast that message to all connected clients
+    wsServer.clients.forEach(function (client) {
+      //client.send(msg,JSON);
+      client.send(""+msg);
+      //print(msg)
+      console.log(count);
+      count++;
+
+    });
+  });
+});
+
+console.log(new Date() + " Server is listening on port " + PORT);
